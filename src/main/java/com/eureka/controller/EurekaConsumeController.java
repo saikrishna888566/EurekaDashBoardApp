ec6fbb232392365ca3d2f4e672595e9fe240b910
kishore <kishorjava91@gmail.com>
2020-05-15 12:33:02 +0530

    my first application

---
+++ src/main/java/com/eureka/controller/EurekaConsumeController.java
@@ -0,0 +1,11 @@
+package com.eureka.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EurekaConsumeController {
+	
+	
+	
+
+}
---
+++ src/test/java/com/eureka/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.eureka;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
